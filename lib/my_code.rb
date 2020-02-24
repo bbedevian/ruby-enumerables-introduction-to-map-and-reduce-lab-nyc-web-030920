df07de6790bf3a4dfa74c9ddc1739ba387993aba
Brett Bedevian <brettbedevian@gmail.com>
2020-02-24 16:20:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,7 +42,7 @@ def reduce_to_total(source_array, starting_point=0)
   result = starting_point
   i = 0
   while i < source_array.length do
-    new += source_array[i]
+    result += source_array[i]
     i += 1
   end
   result
